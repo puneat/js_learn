85eecde9e9784ccc2d1d0fb08f51f4f9dea1613a
puneat <punitm.916@gmail.com>
2020-05-08 20:19:45 +0530

    Operators and their types including typeof

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ if (fullAge==false) {
  * 
  * 
  * 
- */
+ 
 
  var firstName='puneet';
  var age=21;
@@ -45,3 +45,30 @@ alert(age);
 
 var lastName=prompt('enter last name');
 console.log(firstName+' '+lastName);
+*/
+
+// operators
+var year, yearPuneet, yearPunit;
+year=2020;
+var agePunit=20;
+var agePuneet=21;
+yearPuneet =year-agePuneet;
+yearPunit=year-agePunit;
+
+
+console.log(yearPuneet);
+console.log(year*2);
+console.log(year%3);
+console.log(year/2);
+
+//logical operators
+
+var whoIsOlder=agePuneet<agePunit;
+console.log(whoIsOlder);
+
+//typeof operators
+console.log(typeof whoIsOlder);
+console.log(typeof agePuneet);
+console.log(typeof 'hello')
+var x;
+console.log(typeof x);
